34beb446f35b29c5ef4a952d6fa8783d88fcc53c
Zoya <112046214+zoyaumar@users.noreply.github.com>
2025-09-04 21:06:32 -0400

    Render fixes

--- chat-frontend/src/api.ts
+++ chat-frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { TokenResponse, Message, SentimentResult, SummaryResult } from "./types";
 
-const API = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL  });
 
 // Attach JWT if available
 API.interceptors.request.use((req) => {
